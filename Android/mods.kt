b5fdb05514f7ed9268180803bc8fcc2948021a02
mohi0 <88849405+mohi0@users.noreply.github.com>
2024-10-02 19:40:45 +0530

    dashed border mods.kt
    a modifier

--- Android/mods.kt
+++ Android/mods.kt
@@ -7,3 +7,12 @@ fun Modifier.dottedBorder(color: Color, dotGap: Float = 10f) : Modifier{
     }
     return this then mod
 }
+
+@Composable
+fun Modifier.dashedBorder(color: Color, dashLength: Float = 20f, dashGap: Float = 20f): Modifier {
+    val dashedStroke = Stroke(5f, pathEffect = PathEffect.dashPathEffect(floatArrayOf(dashLength, dashGap), 1f))
+    val mod = drawBehind {
+        drawRoundRect(color, style = dashedStroke)
+    }
+    return this then mod
+}
